68b5e9da94815af5623a4b3f5a9d14937dd34e01
Eddy1108 <edvardh@hotmail.no>
2023-06-28 00:45:15 +0200

    Add project files.

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PersonModel
+    {
+        /// <summary>
+        /// First Name of Person
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last Name of Person
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Email belonging to Person
+        /// </summary>
+        public string EmailAdress { get; set; }
+        /// <summary>
+        /// Cellphone number belonging to Person
+        /// </summary>
+        public string CellPhoneNumber { get; set; }
+    }
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        /// <summary>
+        /// List of all members of the team
+        /// </summary>
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>(); //Instantiate List
+        
+        /// <summary>
+        /// Name of the Team
+        /// </summary>
+        public string TeamName { get; set; }
+
+        public TeamModel()
+        {
+            //Instantiate List
+            //TeamMembers = new List<Person>();
+        }
+    }
+}
