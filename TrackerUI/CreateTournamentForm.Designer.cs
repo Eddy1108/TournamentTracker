decab3d83142f884680e1699dc3675d65f69145d
Eddy1108 <edvardh@hotmail.no>
2023-06-28 01:45:03 +0200

    Create Tournament Form

---
+++ TrackerUI/CreateTournamentForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace TrackerUI
+{
+    partial class CreateTournamentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tournamentLabel = new Label();
+            SuspendLayout();
+            // 
+            // tournamentLabel
+            // 
+            tournamentLabel.AutoSize = true;
+            tournamentLabel.Font = new Font("Segoe UI Light", 28F, FontStyle.Regular, GraphicsUnit.Point);
+            tournamentLabel.ForeColor = Color.FromArgb(51, 153, 255);
+            tournamentLabel.Location = new Point(25, 22);
+            tournamentLabel.Name = "tournamentLabel";
+            tournamentLabel.Size = new Size(217, 51);
+            tournamentLabel.TabIndex = 1;
+            tournamentLabel.Text = "Tournament:";
+            tournamentLabel.Click += tournamentLabel_Click;
+            // 
+            // CreateTournamentForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(655, 517);
+            Controls.Add(tournamentLabel);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.FromArgb(51, 153, 255);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "CreateTournamentForm";
+            Text = "Create Tournament";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label tournamentLabel;
+    }
+}
